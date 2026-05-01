5d16dbfc9e4bcef188581f1d8f20f90a11be00bf
Davi Castello Branco Tavares de Oliveira <davicbtoliveira@gmail.com>
2026-05-01 01:30:56 -0400

    FEAT: handle the `.gatorconfig.json` writing

---
+++ internal/config/write_config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func write(cfg Config) error {
+
+	byteArray, err := json.MarshalIndent(cfg, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	os.WriteFile(filePath, byteArray, 0644)
+	return nil
+}
