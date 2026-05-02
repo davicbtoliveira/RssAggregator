daf32508e7b64ed990a88e0f52808a7395640f2d
Davi Castello Branco Tavares de Oliveira <davicbtoliveira@gmail.com>
2026-05-02 15:54:57 -0400

    FIX: setting the `currentUserName` before calling `.write`

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,8 +5,8 @@ type Config struct {
 	CurrentUserName string `json:"current_user_name"`
 }
 
-func (cfg Config) SetUser() error {
-	cfg.CurrentUserName = "dcbto"
+func (cfg *Config) SetUser(user string) error {
+	cfg.CurrentUserName = user
 
 	if err := write(cfg); err != nil {
 		return err
