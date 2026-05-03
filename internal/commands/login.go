34130df745bddd75c195796f3ce8388def456677
Davi Castello Branco Tavares de Oliveira <davicbtoliveira@gmail.com>
2026-05-02 22:24:03 -0400

    FEAT: Handling login using the DB

--- internal/commands/login.go
+++ internal/commands/login.go
@@ -1,10 +1,19 @@
 package commands
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+)
 
 func HandlerLogin(s *State, cmd Command) error {
 	if len(cmd.Args) == 0 {
 		return fmt.Errorf("not enought arguments")
+	}
+
+	if _, err := s.Db.GetUser(context.Background(), cmd.Args[0]); err != nil {
+		fmt.Printf("user %v doesn't exists in the database\n", cmd.Args[0])
+		os.Exit(1)
 	}
 
 	if err := s.Cfg.SetUser(cmd.Args[0]); err != nil {
