f3f1246b51081cab8dc2c583497e0feda9417398
Davi Castello Branco Tavares de Oliveira <davicbtoliveira@gmail.com>
2026-05-02 22:26:09 -0400

    FEAT: Added `config.Database` to `state{}` to use the DB

--- internal/commands/state.go
+++ internal/commands/state.go
@@ -1,7 +1,11 @@
 package commands
 
-import "github.com/davicbtoliveira/rss_aggregator/internal/config"
+import (
+	"github.com/davicbtoliveira/rss_aggregator/internal/config"
+	"github.com/davicbtoliveira/rss_aggregator/internal/database"
+)
 
 type State struct {
+	Db  *database.Queries
 	Cfg *config.Config
 }
