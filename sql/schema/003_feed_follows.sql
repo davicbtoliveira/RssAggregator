d5fe2f4caf61105cb6b6cf051c7c51f964316944
davi.oliveira <davi.oliveira@ernestoborges.com.br>
2026-05-05 16:06:55 -0400

    CHORE: `feed_follows` migration

---
+++ sql/schema/003_feed_follows.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+CREATE TABLE feed_follows (
+  id UUID PRIMARY KEY NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  user_id UUID UNIQUE NOT NULL REFERENCES users(id)
+                  ON DELETE CASCADE,
+  feed_id UUID UNIQUE NOT NULL REFERENCES feeds(id)
+                  ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE feed_follows;
