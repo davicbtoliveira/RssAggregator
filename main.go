2c47e28fc36299dfc488c262817490d349bb11b4
davi.oliveira <davi.oliveira@ernestoborges.com.br>
2026-05-05 17:07:22 -0400

    FEAT: registering `follow` and `following` commands

--- main.go
+++ main.go
@@ -38,6 +38,8 @@ func main() {
 	cmds.Register("agg", commands.HandlerAgg)
 	cmds.Register("addfeed", commands.HandlerAddFeed)
 	cmds.Register("feeds", commands.HandlerFeeds)
+	cmds.Register("follow", commands.HandlerFollow)
+	cmds.Register("following", commands.HandlerFollowing)
 
 	arguments := os.Args
 	if len(arguments) < 2 {
