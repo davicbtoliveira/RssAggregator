67628af65477eaeaf16dc21ca1112360f619c9e2
davi.oliveira <davi.oliveira@ernestoborges.com.br>
2026-05-05 17:09:12 -0400

    CHORE: `GetFeedsForUser` query to fetch all followed feeds from a username

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -16,3 +16,12 @@ SELECT inserted_feed_follow.*,
 FROM inserted_feed_follow
 INNER JOIN users ON inserted_feed_follow.user_id = users.id
 INNER JOIN feeds ON inserted_feed_follow.feed_id = feeds.id;
+
+-- name: GetFeedFollowsForUser :many
+SELECT 
+  ff.*,
+  f.name AS feed_name
+FROM feed_follows ff
+INNER JOIN users u ON ff.user_id = u.id
+INNER JOIN feeds f ON ff.feed_id = f.id
+WHERE u.name = $1;
