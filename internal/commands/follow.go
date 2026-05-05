ae4142e33467bf086c37ff0a2ce658c5efc552c3
davi.oliveira <davi.oliveira@ernestoborges.com.br>
2026-05-05 16:07:26 -0400

    FEAT: `HandlerFollow` to handle the follow command

---
+++ internal/commands/follow.go
@@ -0,0 +1,42 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/davicbtoliveira/rss_aggregator/internal/database"
+	"github.com/google/uuid"
+)
+
+func HandlerFollow(s *State, cmd Command) error {
+	if len(cmd.Args) < 1 {
+		return fmt.Errorf("not enough arguments for follow command")
+	}
+
+	user, err := s.Db.GetUser(context.Background(), s.Cfg.CurrentUserName)
+	if err != nil {
+		return err
+	}
+
+	feed, err := s.Db.GetFeedFromUrl(context.Background(), cmd.Args[0])
+	if err != nil {
+		return err
+	}
+
+	follow_feed, err := s.Db.CreateFeedFollow(context.Background(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		UserID:    user.ID,
+		FeedID:    feed.ID,
+	})
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(follow_feed.FeedName)
+	fmt.Println(follow_feed.UserName)
+
+	return nil
+}
