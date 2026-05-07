a8c0198252f8d73227715cdc1ab2cb5db10b5915
davi.oliveira <davi.oliveira@ernestoborges.com.br>
2026-05-07 17:07:14 -0400

    chore(feeds query): added `GetNextFeedToFetch` to return the next feed that should be fetch posts from

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -22,4 +22,9 @@ WHERE url = $1;
 -- name: MarkFeedFetched :exec
 UPDATE feeds
 SET last_fetched_at = updated_at
-WHERE id = $1
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT *
+FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST;
