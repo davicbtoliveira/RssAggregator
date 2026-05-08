dde45edcc2c97d5f1fa8b83f56917567e713f29f
davi.oliveira <davi.oliveira@ernestoborges.com.br>
2026-05-08 15:16:21 -0400

    feat(agg): added time to fetch feed in `agg` command

--- internal/commands/agg.go
+++ internal/commands/agg.go
@@ -3,16 +3,47 @@ package commands
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/davicbtoliveira/rss_aggregator/internal/rss"
 )
 
-func HandlerAgg(s *State, cmd Command) error {
-	rssfeed, err := rss.FetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+func scrapeFeeds(s *State) error {
+	nextFetch, err := s.Db.GetNextFeedToFetch(context.Background())
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("%+v", rssfeed)
+	if err := s.Db.MarkFeedFetched(context.Background(), nextFetch.ID); err != nil {
+		return err
+	}
+
+	rssFeed, err := rss.FetchFeed(context.Background(), nextFetch.Url)
+	if err != nil {
+		return err
+	}
+
+	for _, v := range rssFeed.Channel.Item {
+		fmt.Println(v.Title)
+	}
+
 	return nil
+}
+
+func HandlerAgg(s *State, cmd Command) error {
+	if len(cmd.Args) < 1 {
+		return fmt.Errorf("not enough arguments to fetch feed")
+	}
+
+	timeBetweenReqs, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Collecting feeds every ", timeBetweenReqs)
+
+	ticker := time.NewTicker(timeBetweenReqs)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
 }
